8ffbdb20596fb6cae9af7b5dce9c30041c13fdbd
Ekansh Harshad Mahajan <145318311+CSE-C70-Mahajan@users.noreply.github.com>
2024-06-08 16:59:59 +0530

    Update booksdb.js

--- router/booksdb.js
+++ router/booksdb.js
@@ -1,5 +1,5 @@
 let books = {
-      1: {"author": "Chinua Achebe", "title": "Things Fall Apart", "reviews": {"user1":"This is a wonderful book!"} },
+      1: {"author": "Chinua Achebe", "title": "Things Fall Apart", "reviews": {} },
       2: {"author": "Hans Christian Andersen", "title": "Fairy tales", "reviews": {} },
       3: {"author": "Dante Alighieri", "title": "The Divine Comedy", "reviews": {} },
       4: {"author": "Unknown", "title": "The Epic Of Gilgamesh", "reviews": {} },
